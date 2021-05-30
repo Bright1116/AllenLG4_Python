7573200aeebef6eeecfdacaf3b311f40bbdf453f
Bright1116 <1126983364@qq.com>
2021-05-30 18:10:48 +0800

    202105301810提交

--- HomeWork/P_20201110/enterprise_wechat.py
+++ HomeWork/P_20201110/enterprise_wechat.py
@@ -9,19 +9,27 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 desired_caps = {
-        "automationName": "UiAutomator2",
-        "platformName": "Android",
-        "platformVersion": "10",  # "10" 、"7.1.2"
-        "deviceName": "huawei",
-        "appPackage": "com.tencent.wework",  # com.lemon.lemonban , com.eg.android.AlipayGphone
-        "appActivity": ".launch.LaunchSplashActivity",
-        # com.lemon.lemonban.activity.WelcomeActivity , com.eg.android.AlipayGphone.AlipayLogin
-        "noReset": True
-    }
+    "automationName": "UiAutomator2",
+    "platformName": "Android",
+    "platformVersion": "10",  # "10" 、"7.1.2"
+    "deviceName": "huawei",
+    "appPackage": "com.tencent.wework",  # com.lemon.lemonban , com.eg.android.AlipayGphone
+    "appActivity": ".launch.LaunchSplashActivity",
+    # com.lemon.lemonban.activity.WelcomeActivity , com.eg.android.AlipayGphone.AlipayLogin
+    "noReset": True
+}
 
 # 跟appium建立连接，然后再把启动参数发过去。
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+# 点击【通讯录】按钮
 loc_addressList_button = (MobileBy.XPATH, "//*[@text='通讯录']")
 WebDriverWait(driver, 30).until(ec.visibility_of_element_located(loc_addressList_button))
 driver.find_element(*loc_addressList_button).click()
 
+# 点击【添加成员】
+loc_add_member = (MobileBy.ANDROID_UIAUTOMATOR, 'new UiScrollable(new UiSelector()'
+                                                '.scrollable(true).instance(0))'
+                                                '.scrollIntoView(new UiSelector()'
+                                                '.text("添加成员").instance(0));')
+WebDriverWait(driver, 30).until(ec.visibility_of_element_located(loc_add_member))
+driver.find_element(*loc_add_member).click()
